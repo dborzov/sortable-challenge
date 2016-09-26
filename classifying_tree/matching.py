bb0108c8d7268cfd3435a3b79815a5466278f78b
Dmitry Borzov <tihoutrom@gmail.com>
2016-09-25 20:27:31 -0400

    Traverse all possible separator combinations

--- classifying_tree/matching.py
+++ classifying_tree/matching.py
@@ -1,10 +1,16 @@
 import re
 
 def extract_tokens(label):
-    de_camel_cased = " ".join([t.lower() for t in re.split('(\B[A-Z][a-z]+)', label) if not len(t)==0])
+    de_camel_cased = " ".join([t.lower() for t in re.split('(\B[A-Z][a-z]*)', label) if not len(t)==0])
     tokens = [t for t in de_camel_cased.replace('-',' ').split()]
     return tokens
 
+SEPARATORS = [" ", "-", "", "_"]
 def generate_variants(tokens):
-    separators = [" ", "-", "", "_"]
-    return set([sep.join(tokens) for sep in separators])
+    if len(tokens) <= 1:
+        return tokens
+    variants = set()
+    for subvariant in generate_variants(tokens[1:]):
+        for sep in SEPARATORS:
+            variants.add(tokens[0]+sep+subvariant)
+    return variants
